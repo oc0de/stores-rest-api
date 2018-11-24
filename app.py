c047ef6378b21721cf6eb088b2bfb3ca9acb0667
3hsan <3hsan.v@gmail.com>
2018-11-24 15:35:31 -0600

    Added home page

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Api
 from flask_jwt import JWT
 
@@ -8,6 +8,7 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+from resources.home import Home
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
@@ -17,6 +18,10 @@ app.secret_key = 'ehsan'
 
 api = Api(app)
 jwt = JWT(app, authenticate, identity)
+
+@app.route("/")
+def hello():
+    return render_template('home.html')
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
