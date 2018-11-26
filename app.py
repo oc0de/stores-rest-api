c92e452f1f8413cc4a4d19af352f108f568f1d27
3hsan <3hsan.v@gmail.com>
2018-11-25 21:24:25 -0600

    add user resources

--- app.py
+++ app.py
@@ -5,10 +5,9 @@ from flask_restful import Api
 from flask_jwt import JWT
 
 from security import authenticate, identity
-from resources.user import UserRegister
+from resources.user import UserRegister, User
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-from resources.home import Home
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
@@ -28,7 +27,11 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
+api.add_resource(User, '/user/<int:user_id>')
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 if __name__ == '__main__':
     from db import db
